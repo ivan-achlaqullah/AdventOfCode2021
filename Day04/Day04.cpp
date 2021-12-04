be4c88e360d9a1d58ae025a26e4ab0863f80ccad
Ivan Achlaqullah <ivan9aks2011@gmail.com>
2021-12-04 16:54:35 +0700

    Day 04 Part 2

--- Day04/Day04.cpp
+++ Day04/Day04.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <regex>
+#include <unordered_set>
 
 struct membingo
 {
@@ -109,13 +110,28 @@ winbingo find_vertical(std::vector<membingo>& board) {
     return result;
 }
 
+void find_vertical(std::vector<membingo>& board, std::vector<winbingo>& win_list) {
+    std::vector<long> order = vertical_order(board.size());
+    for (long i = 0; i < order.size(); i += 5) {
+        winbingo result;
+        result.bingo = board[order[i]].mark &&
+            board[order[i + 1]].mark &&
+            board[order[i + 2]].mark &&
+            board[order[i + 3]].mark &&
+            board[order[i + 4]].mark;
+        if (!result.bingo) continue;
+        result.board = order[i] / 25;
+        win_list.push_back(result);
+    }
+}
+
 winresult winner_board(const std::vector<long>& drawn, std::vector<membingo>& board) {
     winresult result;
     for (long i = 0; i < drawn.size(); i++) {
         for (auto& j : board) {
             if (j.number == drawn[i]) j.mark = true;
         }
-        if (i < 5) continue;
+        if (i < 4) continue;
         auto win = find_horizontal(board);
         if (!win.bingo) win = find_vertical(board);
         if (win.bingo) {
@@ -125,6 +141,30 @@ winresult winner_board(const std::vector<long>& drawn, std::vector<membingo>& bo
         }
     }
     return result;
+}
+
+winresult last_board(const std::vector<long>& drawn, std::vector<membingo>& board) {
+    winresult last_result;
+    std::unordered_set<long> past_winner;
+    for (long i = 0; i < drawn.size(); i++) {
+        for (auto& j : board) {
+            if (j.number == drawn[i]) j.mark = true;
+        }
+        if (i < 4) continue;
+        std::vector<winbingo> win_list;
+        find_horizontal(board, win_list);
+        find_vertical(board, win_list);
+        for (auto& j : win_list) {
+            if (j.bingo && past_winner.find(j.board) == past_winner.end()) {
+                winresult temp;
+                temp.board = j.board;
+                temp.last_num = drawn[i];
+                past_winner.insert(j.board);
+                last_result = temp;
+            }
+        }
+    }
+    return last_result;
 }
 
 int main()
@@ -157,5 +197,34 @@ int main()
     }
     std::cout << "Total Unmarked : " << total_unmarked << "\n";
     std::cout << "Last drawn number : " << bingo.last_num << "\n";
-    std::cout << "Result for Part 1 : " << total_unmarked * bingo.last_num << "\n";
+    std::cout << "Result for Part 1 : " << total_unmarked * bingo.last_num << "\n\n";
+
+    // Clear all mark for Part 2
+    for (auto& i : bingo_board) {
+        i.mark = false;
+    }
+
+    auto last = last_board(drawn_num, bingo_board);
+    std::cout << "Last winner found at board " << last.board + 1 << "\n";
+    
+    // Clear all mark again, then mark it again until it reach last_num
+    total_unmarked = 0;
+    for (auto& i : bingo_board) {
+        i.mark = false;
+    }
+    for (auto i : drawn_num) {
+        for (auto& j : bingo_board) {
+            if (j.number == i) j.mark = true;
+        }
+        if (i == last.last_num) break;
+    }
+    
+    for (long i = 0; i < 25; i++) {
+        long j = last.board * 25 + i;
+        if (bingo_board[j].mark) continue;
+        total_unmarked += bingo_board[j].number;
+    }
+    std::cout << "Total Unmarked : " << total_unmarked << "\n";
+    std::cout << "Last drawn number : " << last.last_num << "\n";
+    std::cout << "Result for Part 1 : " << total_unmarked * last.last_num << "\n\n";
 }
