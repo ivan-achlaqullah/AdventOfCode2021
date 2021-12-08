7bf05c14c3b6f3b1e2ee7941f51e34723c445d9e
Ivan Achlaqullah <ivan9aks2011@gmail.com>
2021-12-08 11:53:57 +0700

    Preparation for Day 08

---
+++ Day08/Day08.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <regex>
+#include <unordered_map>
+#include <algorithm>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
